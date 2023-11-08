528bad33055641ee0c52d618a777b292925b9946
JokerMSD <igorpn2004@gmail.com>
2023-11-08 18:12:16 -0300

    feat: create DarkModeContext.jsx

---
+++ src/providers/DarkModeContext.jsx
@@ -0,0 +1,40 @@
+import { createContext } from "react";
+
+const DarkModeContext = createContext({});
+
+const DarkModeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedDarkMode = localStorage.getItem("darkMode");
+    return savedDarkMode ? JSON.parse(savedDarkMode) : false;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  const handleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
+  return (
+    <DarkModeContext.Provider value={
+        { 
+        darkMode,
+        handleDarkMode 
+        }}>
+
+      {children}
+      
+    </DarkModeContext.Provider>
+  );
+};
+
+export { DarkModeContext, DarkModeProvider };
