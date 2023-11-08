2c523626e609c0692902f065f882eb447f8ab767
JokerMSD <igorpn2004@gmail.com>
2023-11-08 19:53:46 -0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { LandingPage } from "./pages/LandingPage/LandingPage"
-
+import { LandingPage } from "./pages/LandingPage/LandingPage";
+import { DarkModeProvider } from "./providers/DarkModeContext";
+import { PasswordProvider } from "./providers/PasswordStateContext";
+import { UserProvider } from "./providers/UserStateContext";
 
 function App() {
-
   return (
     <>
-    <LandingPage/>
+      <DarkModeProvider>
+        <UserProvider>
+          <PasswordProvider>
+            <LandingPage />
+          </PasswordProvider>
+        </UserProvider>
+      </DarkModeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
