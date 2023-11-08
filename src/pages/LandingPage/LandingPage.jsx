5f8bf792898651113133e0bb332f4a8834cff167
JokerMSD <igorpn2004@gmail.com>
2023-11-08 19:54:18 -0300

    update LandingPage.jsx

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,17 @@
-import { Header } from "../../components/Header/Header"
-
+import { Header } from "../../components/Header/Header";
+import { LoginForm } from "../../components/forms/LoginForm";
+import Style from "./style.module.scss";
 
 export const LandingPage = () => {
-    return (
-        <>
-        <Header/>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <section className={Style.loginSection}>
+        <div className={Style.loginFormContainer}>
+          <LoginForm />
+        </div>
+        <div className={Style.loginSection}></div>
+      </section>
+    </>
+  );
+};
