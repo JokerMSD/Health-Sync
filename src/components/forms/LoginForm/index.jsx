24510b15224e0ae758152b45b05de94989e29ddb
JokerMSD <igorpn2004@gmail.com>
2023-11-09 16:32:38 -0300

    update src/components/forms/LoginForm/index.jsx

--- src/components/forms/LoginForm/index.jsx
+++ src/components/forms/LoginForm/index.jsx
@@ -1,7 +1,8 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Input } from "../../Input";
 import Style from "./style.module.scss";
 import { UserContext } from "../../../providers/UserStateContext";
+import { useNavigate } from "react-router";
 
 export const LoginForm = () => {
   const {
@@ -18,49 +19,44 @@ export const LoginForm = () => {
       className={Style.loginFormContainer}
     >
       <div className={Style.inputDiv}>
-      <Input
-        className={Style.inputLogin}
-        type="email"
-        id="email"
-        label="Email"
-        noHaveSlash={true}
-        noEye={true}
-        autoComplete="username"
-        placeholder="Digite seu email"
-        error={loginErrors.email}
-        {...loginRegister("email")}
-      />
-
-      <Input
-        className={Style.inputLogin}
-        id="password"
-        type="password"
-        label="Senha"
-        autoComplete="current-password"
-        placeholder="Digite sua senha"
-        error={loginErrors.password}
-        {...loginRegister("password")}
-      />
-
-      <div className={Style.checkBoxContainer}>
-        <input 
-        className={Style.checkBox} 
-        id="remember" 
-        type="checkbox" 
+        <Input
+          className={Style.inputLogin}
+          type="email"
+          id="email"
+          label="Email"
+          noHaveSlash={true}
+          noEye={true}
+          autoComplete="username"
+          placeholder="Digite seu email"
+          error={loginErrors.email}
+          {...loginRegister("email")}
         />
-        <label 
-        className={Style.checkBoxLabel}
-        htmlFor="remember"
-        >Entrar automaticamente</label>
+
+        <Input
+          className={Style.inputLogin}
+          id="password"
+          type="password"
+          label="Senha"
+          autoComplete="current-password"
+          placeholder="Digite sua senha"
+          error={loginErrors.password}
+          {...loginRegister("password")}
+        />
+
+        <div className={Style.checkBoxContainer}>
+          <input className={Style.checkBox} id="remember" type="checkbox" />
+          <label className={Style.checkBoxLabel} htmlFor="remember">
+            Entrar automaticamente
+          </label>
+        </div>
       </div>
 
-      </div>
-
-      <button 
-      className={Style.loginBtn}
-      disabled={loading} 
-      type="submit"
-      >Entrar
+      <button
+        className={Style.loginBtn}
+        disabled={loading}
+        type="submit"
+      >
+        Entrar
       </button>
     </form>
   );
